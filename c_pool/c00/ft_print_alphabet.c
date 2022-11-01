1c05eba22a05bd1587f78e685516914ed14b2aa9
Aleksandr Kravtsov <nyasho4ka@gmail.com>
2022-11-01 23:55:03 +0500

    feat: ft_print_alphabet

---
+++ c_pool/c00/ft_print_alphabet.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_alphabet(void)
+{
+    char first_letter = 97;
+    char last_letter = first_letter + 25;
+    for (char letter = first_letter; letter <= last_letter; letter++)
+    {
+        ft_putchar(letter);
+    }
+}
+
+int main()
+{
+    ft_print_alphabet();
+}
