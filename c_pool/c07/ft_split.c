9562901309577bfd047a4003b525519fa33e7e93
Aleksandr Kravtsov <nyasho4ka@gmail.com>
2022-11-13 13:39:39 +0500

    feat: ft_split

--- c_pool/c07/ft_split.c
+++ c_pool/c07/ft_split.c
@@ -13,39 +13,6 @@ int is_start_with(char* str, char* charset)
     return 1;
 }
 
-int* get_indecies(char* str, char* charset, int charset_size)
-{
-    int char_pos = 0;
-
-    while (str[char_pos])
-    {
-        char_pos++;
-    }
-    int* indecies = malloc(sizeof(int) * char_pos);
-
-    char_pos = 0;
-    int begin_pos = 0;
-    int indecies_counter = 0;
-    while (str[char_pos])
-    {
-        if (is_start_with(str + char_pos, charset))
-        {
-            if (char_pos != begin_pos)
-            {
-                indecies[indecies_counter] = begin_pos;
-                indecies_counter++;
-            }
-            char_pos += charset_size;
-        }
-        else
-        {
-            char_pos++;
-        }
-    }
-
-    return indecies;
-}
-
 int ft_strlen(char* str)
 {
     int i = 0;
@@ -56,15 +23,66 @@ int ft_strlen(char* str)
     return i;
 }
 
+char** get_indecies(char* str, char* charset, int charset_size)
+{
+    int char_pos = 0;
+    while (str[char_pos])
+        char_pos++;
+    
+    // cannot be more than half of char seps;
+    int indecies_count = char_pos % 2 == 0 ? char_pos / 2 + 1: char_pos / 2 + 2;
+    char** indecies = malloc(sizeof(char*) * indecies_count);
+
+    char_pos = 0;
+    int begin_word_pos = 0;
+    int index = 0;
+
+    while (str[char_pos])
+    {
+        if (is_start_with(str + char_pos, charset))
+        {
+            if (char_pos != begin_word_pos)
+            {
+                indecies[index] = str + begin_word_pos;
+                index++;
+            }
+            char_pos += charset_size;
+            begin_word_pos = char_pos;
+        }
+        else
+        {
+            char_pos++;
+        }
+    }
+    // Add last word
+    if (str[begin_word_pos])
+    {
+        indecies[index] = str + begin_word_pos;
+        index++;
+    }
+
+    indecies[index] = 0;
+
+    return indecies;
+}
+
 char** ft_split(char* str, char* charset)
 {
     int charset_size = ft_strlen(charset);
-    int word_count = get_word_count(str, charset, charset_size);
-    char** words = malloc(sizeof(char*) * word_count);
-
+    return get_indecies(str, charset, charset_size);
 }
 
 int main()
 {
-    printf("str: abbbdboob, charset: b, word_count: %d\n", get_word_count("abbbdboob", "b", 1));
+    char* str = "abdddbdbdoob";
+    char* charset = "bd";
+    char** spl = ft_split(str, charset);
+
+    printf("str: %s, charset: %s\n", str, charset);
+    int i = 0;
+    while (spl[i] != 0)
+    {
+        printf("index: %d, char: %c\n", i, spl[i][0]);
+        i++;
+    }    
 }
