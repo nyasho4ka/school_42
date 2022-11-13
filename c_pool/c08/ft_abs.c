432d211f1ac7c4bdd5d32682e810715bd548637d
Aleksandr Kravtsov <nyasho4ka@gmail.com>
2022-11-13 17:54:51 +0500

    feat: ft_abs

---
+++ c_pool/c08/ft_abs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "ft_abs.h"
+
+int main()
+{
+    printf("ABS(%d), %d\n", 0, ABS(0));
+    printf("ABS(%d), %d\n", -5, ABS(-5));
+    printf("ABS(%d), %d\n", 5, ABS(5));
+}
