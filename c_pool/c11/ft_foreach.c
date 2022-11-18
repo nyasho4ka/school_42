1a2f47ad272bf20feaa12e31cd706ca287cd0c1f
Aleksandr Kravtsov <nyasho4ka@gmail.com>
2022-11-18 11:49:14 +0500

    feat: ft_foreach

---
+++ c_pool/c11/ft_foreach.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+
+const int ZERO_DIGIT_POSITION = 48;
+const int MAX_INT_DIGITS = 10;
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void print_digit(int digit)
+{
+    ft_putchar(digit + ZERO_DIGIT_POSITION);
+}
+
+void ft_putnbr(int nb)
+{
+    int digit_count = 1;
+    int denominator = 10;
+    while(nb / denominator != 0)
+    {
+        digit_count++;
+        denominator *= 10;
+        if (digit_count == MAX_INT_DIGITS)
+        {
+            break;
+        }
+    }
+
+    if (nb < 0)
+    {
+        nb *= -1;
+        ft_putchar('-');
+    }
+
+    for (int i = digit_count - 1; i > 0; i--)
+    {
+        int denominator = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            denominator *= 10;
+        }
+        print_digit(nb / denominator);
+        nb = nb % denominator;
+    }
+    print_digit(nb);
+    ft_putchar('\n');
+}
+
+
+void ft_foreach(int *tab, int length, void(*f)(int))
+{
+    for (int i = 0; i < length; i++)
+    {
+        (*f)(tab[i]);
+    }
+}
+
+int main()
+{
+    int tab[5] = {-11, 123, 765, -9, -768543};
+    ft_foreach(tab, 5, &ft_putnbr);
+    return (0);
+}
