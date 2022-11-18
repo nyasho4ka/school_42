bcc5c5a942c187ca1ce04884eee92e29d56eb2e1
Aleksandr Kravtsov <nyasho4ka@gmail.com>
2022-11-18 15:46:53 +0500

    fix: ft_create_elem

--- c_pool/c12/ft_create_elem.c
+++ c_pool/c12/ft_create_elem.c
@@ -4,7 +4,10 @@
 t_list *ft_create_elem(void *data)
 {
     t_list *elem = (t_list*)malloc(sizeof(t_list));
-    elem->data = data;
-    elem->next = NULL;
+    if (elem)
+    {
+        elem->data = data;
+        elem->next = NULL;
+    }
     return elem;
 }
