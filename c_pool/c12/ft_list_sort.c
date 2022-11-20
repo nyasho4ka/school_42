f70eaa494fa32c479e33690c90b307b271d3150d
Aleksandr Kravtsov <nyasho4ka@gmail.com>
2022-11-20 23:24:38 +0500

    feat: ft_list_sort

--- c_pool/c12/ft_list_sort.c
+++ c_pool/c12/ft_list_sort.c
@@ -18,11 +18,13 @@ void ft_print_list(t_list* list)
     printf("------------------\n");
     while (curr)
     {
-        printf("%s\n", (char*)curr->data);
+        printf("%s, curr: %p, curr->next: %p\n", (char*)curr->data, curr, curr->next);
         curr = curr->next;
     }
     printf("------------------\n");
 }
+
+char buff[20];
 
 void ft_list_sort(t_list** begin_list, int(*cmp)())
 {
@@ -52,12 +54,39 @@ void ft_list_sort(t_list** begin_list, int(*cmp)())
     }
 }
 
+void ft_swap_nodes(t_list** first, t_list** second)
+{
+    t_list* tmp_ptr1 = *first;
+    t_list* tmp_next1 = (*first)->next;
+    t_list* tmp_ptr2 = *second;
+    t_list* tmp_next2 = (*second)->next;
+    tmp_ptr2->next = tmp_next1;
+    tmp_ptr1->next = tmp_next2;
+    *second = tmp_ptr1;
+    *first = tmp_ptr2;
+    read(0, buff, 10);
+}
+
+// todo: using pointer on pointer can't help with neigbour elements
+void ft_list_sort_advanced(t_list** begin_list, int(*cmp)())
+{
+    (void)(*cmp);
+    t_list** curr = begin_list;
+    t_list** inner = &((*curr)->next);
+    while (*inner)
+    {
+        ft_swap_nodes(curr, inner);
+        inner = &((*inner)->next);
+    }
+}
+
 int main()
 {
-    t_list* list = ft_create_elem("hello");
-    list->next = ft_create_elem("world");
-    list->next->next = ft_create_elem("apple");
-    list->next->next->next = ft_create_elem("bolly");
+    t_list* list = ft_create_elem("world");
+    list->next = ft_create_elem("hello");
+    list->next->next = ft_create_elem("bolly");
+    list->next->next->next = ft_create_elem("apple");
+
     ft_print_list(list);
     ft_list_sort(&list, &ft_strcmp);
     ft_print_list(list);
