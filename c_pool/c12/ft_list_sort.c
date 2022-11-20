dbd6f9feec2f6bb1c2f7f73d281d9565ead8f71b
Aleksandr Kravtsov <nyasho4ka@gmail.com>
2022-11-21 01:13:30 +0500

    feat: ft_list_sort

--- c_pool/c12/ft_list_sort.c
+++ c_pool/c12/ft_list_sort.c
@@ -24,8 +24,6 @@ void ft_print_list(t_list* list)
     printf("------------------\n");
 }
 
-char buff[20];
-
 void ft_list_sort(t_list** begin_list, int(*cmp)())
 {
     t_list* curr = *begin_list;
@@ -54,29 +52,62 @@ void ft_list_sort(t_list** begin_list, int(*cmp)())
     }
 }
 
+void ft_swap_neigbours(t_list** first)
+{
+    t_list* after_pair = (*first)->next->next;
+    (*first)->next->next = *first;
+    *first = (*first)->next;
+    (*first)->next->next = after_pair;
+}
+
 void ft_swap_nodes(t_list** first, t_list** second)
 {
     t_list* tmp_ptr1 = *first;
-    t_list* tmp_next1 = (*first)->next;
+    t_list* tmp_next1 = tmp_ptr1->next;
     t_list* tmp_ptr2 = *second;
-    t_list* tmp_next2 = (*second)->next;
+    t_list* tmp_next2 = tmp_ptr2->next;
+
     tmp_ptr2->next = tmp_next1;
     tmp_ptr1->next = tmp_next2;
     *second = tmp_ptr1;
     *first = tmp_ptr2;
-    read(0, buff, 10);
 }
+
+int ft_is_neigbours(t_list** first, t_list** second)
+{
+    return (*first)->next == (*second);
+}
+
 
 // todo: using pointer on pointer can't help with neigbour elements
 void ft_list_sort_advanced(t_list** begin_list, int(*cmp)())
 {
-    (void)(*cmp);
+    if (*begin_list == (void*)(0) || (*begin_list)->next == (void*)(0))
+        return;
+
     t_list** curr = begin_list;
-    t_list** inner = &((*curr)->next);
-    while (*inner)
+    while (*curr)
     {
-        ft_swap_nodes(curr, inner);
-        inner = &((*inner)->next);
+        t_list** inner = &((*curr)->next);
+        while (*inner)
+        {   
+            if ((*cmp)((*inner)->data, (*curr)->data) >= 0)
+            {
+                inner = &(*inner)->next;
+                continue;
+            }
+
+            if (ft_is_neigbours(curr, inner))
+            {
+                ft_swap_neigbours(curr);
+            }
+            else
+            {
+                ft_swap_nodes(curr, inner);
+                inner = &(*inner)->next;
+            }
+        }
+        curr = &(*curr)->next;
     }
 }
 
@@ -86,9 +117,12 @@ int main()
     list->next = ft_create_elem("hello");
     list->next->next = ft_create_elem("bolly");
     list->next->next->next = ft_create_elem("apple");
+    list->next->next->next->next = ft_create_elem("kek");
+    list->next->next->next->next->next = ft_create_elem("prelast");
+    list->next->next->next->next->next->next = ft_create_elem("last");
 
     ft_print_list(list);
-    ft_list_sort(&list, &ft_strcmp);
+    ft_list_sort_advanced(&list, &ft_strcmp);
     ft_print_list(list);
     return (0);
 }
