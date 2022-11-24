4df4969a03afa81f4c620b7a183ce80ddcc3245a
Aleksandr Kravtsov <nyasho4ka@gmail.com>
2022-11-25 02:27:15 +0500

    feat: ft_memchr

---
+++ 00_global/01_libft/ft_memchr.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "libft.h"
+
+void* ft_memchr(const void *s, int c, size_t n)
+{
+    const unsigned char* s_ch = (const unsigned char*)s;
+    while (n-- && s_ch)
+    {
+        if (*s_ch == (unsigned char)c)
+            return (void*)s_ch;
+        s_ch++;
+    }
+    return (void*)(0);
+}
